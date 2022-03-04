c6dcc1a1c355114882829a6c61aa49a0f245a8c7
xalvaine <xalvaine@gmail.com>
2022-03-05 01:34:01 +0300

    [feat] Format code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-import { User } from "./entities/user.entity";
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { User } from './entities/user.entity'
+import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   exports: [TypeOrmModule, UsersService],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
