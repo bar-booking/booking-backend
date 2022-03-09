122a49576a2014643ee6f3082bd3c4588089800e
xalvaine <xalvaine@gmail.com>
2022-03-10 01:36:11 +0300

    [feat] Add certbot

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose -f production.yml run certbot renew \
-&& docker-compose -f production.yml kill -s SIGHUP webserver
+&& docker-compose -f production.yml kill -s SIGHUP nginx
